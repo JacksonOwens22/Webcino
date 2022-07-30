1c47f2c471bb7b23f7e9df2bf2cbec59d77a350f
JacksonOwens22 <77686096+JacksonOwens22@users.noreply.github.com>
2022-07-30 13:02:40 -0500

    Add files via upload

---
+++ View/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link href="CSS/home.css" rel="stylesheet" type="text/css"/>
+        <title>LandScape</title>
+        <!-- <script src="https://code.jquery.com/jquery-latest.min.js"></script> -->
+        <!--<script src="flight.js"></script>-->
+    </head>
+    <body>
+		<header>
+		
+		</header>
+            <main>
+                <h1>Body</h1>
+            </main>
+    </body>
+
+</html>
