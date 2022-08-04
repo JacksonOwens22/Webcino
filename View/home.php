3acab8440b64831153d6334b1f5d5b5d9c02bb39
jackson-owens <jackson-owens@CDEO-OWENSJ.cdol.int>
2022-08-04 16:31:16 -0500

    figuring out how to make the dice stop rolling and do functions

--- View/home.php
+++ View/home.php
@@ -13,83 +13,82 @@
             <main>
                 <div class="dice-container">
                     <div class="dice">
-			<img class="img1" src="Photos/dice6.png">
+			<img class="img1 paused" id="dice1" src="Photos/dice6.png">
                     </div>
 
                     <div class="dice">
-			<img class="img2" src="Photos/dice6.png">
+			<img class="img2 paused" id="dice2" src="Photos/dice6.png">
                     </div>
                     <div class="rollOutcome">
                         <p id="roll1"></p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         <p id="roll2"></p>
                     </div>           
                 </div>
-  
                 <div class="container-grid">
                     <div class="row1">
                         <div class="tile">
                             <button id="rollBtn" class="rollBtn">Roll the Dice</button>
                         </div>
-                        <div class="tile">1</div>
-                        <div class="tile">2</div>
-                        <div class="tile">3</div>  
-                        <div class="tile">4</div>
-                        <div class="tile">5</div>
-                        <div class="tile">6</div>
+                        <div class="tile" id="row1">1</div>
+                        <div class="tile" id="row2">2</div>
+                        <div class="tile" id="row3">3</div>  
+                        <div class="tile" id="row4">4</div>
+                        <div class="tile" id="row5">5</div>
+                        <div class="tile" id="row6">6</div>
                     </div>
                     <div class="row2">
-                        <div class="tile">1</div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>  
-                        <div class="tile"></div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>
+                        <div class="tile" id="column1">1</div>
+                        <div class="tile" id="sqaure11"></div>
+                        <div class="tile" id="sqaure12"></div>  
+                        <div class="tile" id="sqaure13"></div>
+                        <div class="tile" id="sqaure14"></div>
+                        <div class="tile" id="sqaure15"></div>
+                        <div class="tile" id="sqaure16"></div>
                     </div>
                     <div class="row3">
-                        <div class="tile">2</div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>  
-                        <div class="tile"></div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>
+                        <div class="tile" id="column2">2</div>
+                        <div class="tile" id="sqaure21"></div>
+                        <div class="tile" id="sqaure22"></div>  
+                        <div class="tile" id="sqaure23"></div>
+                        <div class="tile" id="sqaure24"></div>
+                        <div class="tile" id="sqaure25"></div>
+                        <div class="tile" id="sqaure26"></div>
                     </div>
                     <div class="row4">
-                        <div class="tile">3</div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>  
-                        <div class="tile"></div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>
+                        <div class="tile" id="column3">3</div>
+                        <div class="tile" id="sqaure31"></div>
+                        <div class="tile" id="sqaure32"></div>  
+                        <div class="tile" id="sqaure33"></div>
+                        <div class="tile" id="sqaure34"></div>
+                        <div class="tile" id="sqaure35"></div>
+                        <div class="tile" id="sqaure36"></div>
                     </div>
                     <div class="row5">
-                        <div class="tile">4</div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>  
-                        <div class="tile"></div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>
+                        <div class="tile" id="column4">4</div>
+                        <div class="tile" id="sqaure41"></div>
+                        <div class="tile" id="sqaure42"></div>  
+                        <div class="tile" id="sqaure43"></div>
+                        <div class="tile" id="sqaure44"></div>
+                        <div class="tile" id="sqaure45"></div>
+                        <div class="tile" id="sqaure46"></div>
                     </div>
                     <div class="row6">
-                        <div class="tile">5</div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>  
-                        <div class="tile"></div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>
+                        <div class="tile" id="column5">5</div>
+                        <div class="tile" id="sqaure51"></div>
+                        <div class="tile" id="sqaure52"></div>  
+                        <div class="tile" id="sqaure53"></div>
+                        <div class="tile" id="sqaure54"></div>
+                        <div class="tile" id="sqaure55"></div>
+                        <div class="tile" id="sqaure56"></div>
                     </div>
                     <div class="row7">
-                        <div class="tile">6</div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>  
-                        <div class="tile"></div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>
-                        <div class="tile"></div>
+                        <div class="tile" id="column6">6</div>
+                        <div class="tile" id="sqaure61"></div>
+                        <div class="tile" id="sqaure62"></div>  
+                        <div class="tile" id="sqaure63"></div>
+                        <div class="tile" id="sqaure64"></div>
+                        <div class="tile" id="sqaure65"></div>
+                        <div class="tile" id="sqaure66"></div>
                     </div>
                 </div>
             </main>
--- gameBoardLogic.js
+++ gameBoardLogic.js
@@ -1,19 +1,39 @@
 'use stric';
 $(document).ready(function() {
     $('#rollBtn').click(rollTheDice);
-    
 });
 
 var rollTheDice = function() {
+        //$('#rollBtn').attr('disabled', true);
         var randomNumber1 = Math.floor(Math.random() * 6) + 1;
         var randomNumber2 = Math.floor(Math.random() * 6) + 1;
         
+        $('#dice1').removeClass('paused');
+        $('#dice2').removeClass('paused');
+        rollingAnimation = setInterval(timer, 2200);
+        
+        
+        
         $('#roll1').text(randomNumber1);
         $('#roll2').text(randomNumber2);
-        
+                
         document.querySelector(".img1").setAttribute("src",
             "Photos/dice" + randomNumber1 + ".png");
             
         document.querySelector(".img2").setAttribute("src",
             "Photos/dice" + randomNumber2 + ".png");
+        
+        selectNumber(randomNumber1, randomNumber2);
+        clearInterval(timer);
 };
+
+var selectNumber = function(randomNumber1, randomNumber2){
+    $('#column' + randomNumber1).css("background-color", "yellow");
+    $('#row' + randomNumber2).css("background-color", "yellow");
+};
+
+function timer() {
+        $('#dice1').addClass('paused');
+        $('#dice2').addClass('paused');
+};
+
